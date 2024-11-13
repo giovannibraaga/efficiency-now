ca43e8452a0b67a3a1c1b23dd9c0fe94459f831f
Giovanni Bernardo <canaltamojuntooooo@gmail.com>
2024-11-12 21:13:32 -0300

    first commit

---
+++ src/test/java/com/efficiencynow/efficiencynow/EfficiencynowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.efficiencynow.efficiencynow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfficiencynowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
